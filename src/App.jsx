f40714354efbf357adb8e05779408e1a449f9b54
ryanmitchellin <ryanvincent999@gmail.com>
2024-07-20 14:30:22 -0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Volunteer from "./components/Volunteer/Volunteer";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Volunteer />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

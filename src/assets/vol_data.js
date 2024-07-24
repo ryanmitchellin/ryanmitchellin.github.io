c6b6cd4f7743fa7378d41ff53288e3e04fe397f0
ryanmitchellin <ryanvincent999@gmail.com>
2024-07-24 15:51:09 -0700

    Re-write volunteer, making it more dynamic

---
+++ src/assets/vol_data.js
@@ -0,0 +1,28 @@
+const Volunteer_Data = [
+  {
+    v_position: "Manager",
+    v_title: "Peer Education, FIC",
+    v_detail:
+      "As a manager at FIC, I managed a team of 30 top-performing students to assist approximately 300 peers per semester in understanding their coursework. Among my tasks were hosting workshops to promote Peer Education facilities and volunteer opportunities, conducting new member interviews, and organizing student leadership training to prepare them as tutors and mentors. I also facilitated regular discussion sessions to ensure continuous improvement and address any challenges faced by the team.",
+  },
+  {
+    v_position: "Tutor & Mentor",
+    v_title: "Peer Education, FIC",
+    v_detail:
+      "As a tutor and mentor at FIC, I supported fellow students through one-on-one academic assistance and mentorship. Among my tasks wereproviding help with various courses, breaking down complex concepts, and offering advice on navigating student life. I conducted drop-insessions and participated in the Peer Education Mentorship Program, focusing on time management, study skills, and communication. Committing to a two-hour weekly shift, I engaged in team projects, attended training sessions, and collaborated with diverse teams. This role enhanced my communication skills, provided professional experience, and offered opportunities for future leadership positions within the program.",
+  },
+  {
+    v_position: "IT Support Technician",
+    v_title: "IT Squad, FIC",
+    v_detail:
+      "As an IT support technician at FIC, I collaborate with otherstudents and alumni to provide comprehensive technical support across the FIC community. My tasks included maintaining computers, printers, projectors, and photocopiers and offering online supportfor issues like activating Computing IDs and troubleshooting Zoom problems. By resolving daily technical issues for staff and students, the IT support technician reduced stress and improved theFIC environment. I also engaged in project collaborations, attended training sessions, and contributed ideas to enhance technical support, committing to two shifts per week for two consecutive semesters.",
+  },
+  {
+    v_position: "Orientation Coordinator",
+    v_title: "Orientation Leader, FIC",
+    v_detail:
+      "As an orientation coordinator at FIC, I welcomed new students and supported them in building relationships within the FIC community. Among my tasks were leading icebreakers, hosting events during Weeks of Welcome, and running community connection sessions. I introduced students to their academic environment, important resources, and peers, helping to ease their worries and make them feel welcome. This role required energy, enthusiasm, and responsibility, involving collaboration with other leaders and professional staff. The experience provided professional development, enhanced my leadership skills, and fostered a fun, inclusive community for new students.",
+  },
+];
+
+export default Volunteer_Data;
--- src/components/Volunteer/Volunteer.jsx
+++ src/components/Volunteer/Volunteer.jsx
@@ -3,6 +3,7 @@ import "./Volunteer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 import { faAnglesLeft } from "@fortawesome/free-solid-svg-icons";
+import Volunteer_Data from "../../assets/vol_data";
 
 const Volunteer = () => {
   return (
@@ -21,70 +22,15 @@ const Volunteer = () => {
         />
       </div>
       <div className="volunteer-sections">
-        <div className="volunteer-sect">
-          <h2>Manager</h2>
-          <h3>Peer Education, FIC</h3>
-          <p>
-            As a manager at FIC, I managed a team of 30 top-performing students
-            to assist approximately 300 peers per semester in understanding
-            their coursework. Among my tasks were hosting workshops to promote
-            Peer Education facilities and volunteer opportunities, conducting
-            new member interviews, and organizing student leadership training to
-            prepare them as tutors and mentors. Additionally, I represented FIC
-            in formal meetings with Members of the Legislative Assembly (MLA),
-            Navitas (FIC) stakeholders, and the SFU faculty department.
-          </p>
-        </div>
-        <div className="volunteer-sect">
-          <h2>Tutor & Mentor</h2>
-          <h3>Peer Education, FIC</h3>
-          <p>
-            As a tutor and mentor at FIC, I supported fellow students through
-            one-on-one academic assistance and mentorship. Among my tasks were
-            providing help with various courses, breaking down complex concepts,
-            and offering advice on navigating student life. I conducted drop-in
-            sessions and participated in the Peer Education Mentorship Program,
-            focusing on time management, study skills, and communication.
-            Committing to a two-hour weekly shift, I engaged in team projects,
-            attended training sessions, and collaborated with diverse teams.
-            This role enhanced my communication skills, provided professional
-            experience, and offered opportunities for future leadership
-            positions within the program.
-          </p>
-        </div>
-        <div className="volunteer-sect">
-          <h2>IT Support Technician</h2>
-          <h3>IT Squad, FIC</h3>
-          <p>
-            As an IT support technician at FIC, I collaborate with other
-            students and alumni to provide comprehensive technical support
-            across the FIC community. My tasks included maintaining computers,
-            printers, projectors, and photocopiers and offering online support
-            for issues like activating Computing IDs and troubleshooting Zoom
-            problems. By resolving daily technical issues for staff and
-            students, the IT support technician reduced stress and improved the
-            FIC environment. I also engaged in project collaborations, attended
-            training sessions, and contributed ideas to enhance technical
-            support, committing to two shifts per week for two consecutive
-            semesters.
-          </p>
-        </div>
-        <div className="volunteer-sect">
-          <h2>Orientation Coordinator</h2>
-          <h3>Orientation Leader, FIC</h3>
-          <p>
-            As an orientation coordinator at FIC, I welcomed new students and
-            supported them in building relationships within the FIC community.
-            Among my tasks were leading icebreakers, hosting events during Weeks
-            of Welcome, and running community connection sessions. I introduced
-            students to their academic environment, important resources, and
-            peers, helping to ease their worries and make them feel welcome.
-            This role required energy, enthusiasm, and responsibility, involving
-            collaboration with other leaders and professional staff. The
-            experience provided professional development, enhanced my leadership
-            skills, and fostered a fun, inclusive community for new students.
-          </p>
-        </div>
+        {Volunteer_Data.map((vol, index) => {
+          return (
+            <div key={index} className="volunteer-sect">
+              <h2>{vol.v_position}</h2>
+              <h3>{vol.v_title}</h3>
+              <p>{vol.v_detail}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
